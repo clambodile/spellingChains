2585411db8118c950e2a61847a8116b42b84f1df
John Jacob <jacobjohnrobert@gmail.com>
2017-05-15 16:07:37 -0500

    swap out Map for Set

--- spellingChain.hs
+++ spellingChain.hs
@@ -1,21 +1,21 @@
 import Data.Char
-import qualified Data.Map as Map
+import qualified Data.Set as Set
 import System.IO
 
 main = do
   contents <- readFile "dictionary.txt"
   let wordList = words $ map toLower contents
-      dictionary = Map.fromList $ zip wordList wordList
+      dictionary = Set.fromList wordList
   putStr $ unlines $ foldl1 longest $ map (flip spellingChain dictionary) wordList
   
   
-spellingChain :: String -> Map.Map String String -> [String]
+spellingChain :: String -> Set.Set String -> [String]
 spellingChain "" _ = []
 spellingChain (x:[]) _ = if x `elem` ['a', 'i', 'o'] then [x:""] else []
 spellingChain word dict =
   let lChain = spellingChain (init word) dict
       rChain = spellingChain (tail word) dict
-      valid = Map.member word dict
+      valid = Set.member word dict
   in if valid then word : (longest lChain rChain) else []
 
 longest :: [a] -> [a] -> [a]
