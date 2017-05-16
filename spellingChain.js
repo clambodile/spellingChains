57ad7bfbecf3f9f121af15bbd1d0903085ad2a8b
Jake F - @samouri <samouri@users.noreply.github.com>
2017-05-16 12:36:31 -0400

    not bad

--- spellingChain.js
+++ spellingChain.js
@@ -3,25 +3,22 @@ const fs = require('fs');
 const longest = list => list.reduce((acc, word) => (acc.length > word.length ? acc : word));
 const isValid = chain => chain.length > 0 && chain.slice(-1)[0].length == 1;
 
-const longestChain = dictionary =>
-	longest(
-		Array.from(dictionary.values()).map(word => checkChain(word, dictionary)).filter(isValid)
-	);
+const longestChain = (dictList, dictSet) =>
+	longest(dictList.map(word => checkChain(word, dictSet)).filter(isValid));
 
-const checkChain = (word, dictionary) => {
+const checkChain = (word, dictSet) => {
 	const wordL = word.slice(0, word.length - 1);
 	const wordR = word.slice(1);
 
-	return !dictionary.has(wordL) && !dictionary.has(wordR)
-		? [word] // base case
-		: [word].concat(longest(checkChain(wordL, dictionary), checkChain(wordR, dictionary)));
+	return !dictSet.has(wordL) && !dictSet.has(wordR)
+		? [word]
+		: longest([word].concat(checkChain(wordL, dictSet)), [word].concat(checkChain(wordR, dictSet)));
 };
 
-const dictionary = new Set(
-	fs
-		.readFileSync('dictionary.txt', 'utf8')
-		.split('\n')
-		.map(str => str.toLowerCase())
-		.concat(['a', 'i', 'o'])
-);
-console.log(longestChain(dictionary));
+const dictionary = fs
+	.readFileSync('dictionary.txt', 'utf8')
+	.split('\n')
+	.map(str => str.toLowerCase())
+	.concat(['a', 'i', 'o']);
+
+console.log(longestChain(dictionary, new Set(dictionary)));
